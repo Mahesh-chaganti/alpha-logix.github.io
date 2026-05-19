a71164155e299bedbe39d6222ee3b7d7f9e8503c
Mahesh <59696075+Mahesh-chaganti@users.noreply.github.com>
2026-05-19 17:48:59 +0530

    Use cheaper dall-e-2 model instead of dall-e-3

--- api/generate.js
+++ api/generate.js
@@ -51,7 +51,7 @@ export default async function handler(req, res) {
       const response = await axios.post(
         'https://api.openai.com/v1/images/generations',
         {
-          model: 'dall-e-3',
+          model: 'dall-e-2',
           prompt: prompt,
           size: '1024x1024',
           response_format: 'b64_json'
